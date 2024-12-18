25725ff29edf92084ce790ec1a29c4a4fb2170a3
Fabimhso <hsofabi05@gmail.com>
2024-12-18 18:17:15 -0300

    Front-End Integration with PHP Back-End

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php 
+
+$livros = [
+    ['id' => 1, 'titulo' => 'A Vida Invisivel de ADDIE LARUE', 'autor' => 'V.E. SCHWAB', 'descricao' => 'A Menina que fez um pacto com a escuridao e foi esquecida por todos'],
+    ['id' => 2, 'titulo' => 'Harry Potter', 'autor' => 'J.K Rowling', 'descricao' => 'O Garoto que foi admitido na escola para bruxos Hogwarts'],
+    ['id' => 3, 'titulo' => 'A Menina que Roubava Livros', 'autor' => 'Markus Zusak', 'descricao' => 'A Garota que encontrou a morte 3 vezes e sobreviveu todas'],
+    ['id' => 4, 'titulo' => 'Crepusculo', 'autor' => 'Stephenie Meyer', 'descricao' => 'O Vampiro que Brilha no Sol'],
+    ['id' => 5, 'titulo' => 'Para Todos os Garotos Que Ja Amei', 'autor' => 'Jenny Han', 'descricao' => 'A Garota que Ama ate o Cara do Gas'],
+    ['id' => 6, 'titulo' => 'Mr. Robot: Red Wheelbarrow', 'autor' => 'Sam Esmail', 'descricao' => 'O Mlk que Hackeou ate tua Mae'],
+    ['id' => 7, 'titulo' => 'O Manual do Mundo', 'autor' => 'Ibere Tenorio', 'descricao' => 'O Manual de Como Hackear o Mundo'],
+    ['id' => 8, 'titulo' => 'Enaldinho - O Livro', 'autor' => 'Enaldo', 'descricao' => 'Livro do YouTuber Enaldinho, Enaldo, Enaldo, Enaldo'],
+    ['id' => 9, 'titulo' => 'Manual de Assassinato para Boas Garotas', 'autor' => 'Holly Jackson', 'descricao' => 'Como assassinar e continuar sendo boazinha e tirar boas notas']
+];
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,27 +55,7 @@
             
             <!-- LIVROS -->
 
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-
-                <div class="flex">
-
-                    <div class="w-1/3">Imagem</div>
-
-                    <div>
-
-                        <a href="/livro.php?" class="font-semibold hover:underline">Titulo</a>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
-
-                    </div>
-
-                </div>
-
-                <div class="text-sm">
-                 Descricao
-                </div>
-
-            </div>
+            <?php foreach($livros as $livro): ?>
 
             <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
 
@@ -65,109 +63,23 @@
 
                     <div class="w-1/3">Imagem</div>
 
-                    <div>
+                    <div class="space-y-1">
 
-                        <a href="/livro.php?" class="font-semibold hover:underline">Titulo</a>
-                        <div class="text-xs italic">Autor</div>
+                        <a href="/livro.php?id=<?=$livro['id']?>" class="font-semibold hover:underline"><?=$livro['titulo']?></a>
+                        <div class="text-xs italic"><?=$livro['autor']?></div>
                         <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
 
                     </div>
 
                 </div>
 
-                <div class="text-sm">
-                 Descricao
+                <div class="text-sm mt-2">
+                <?=$livro['descricao']?>
                 </div>
 
             </div>
 
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-
-                <div class="flex">
-
-                    <div class="w-1/3">Imagem</div>
-
-                    <div>
-
-                        <a href="/livro.php?" class="font-semibold hover:underline">Titulo</a>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
-
-                    </div>
-
-                </div>
-
-                <div class="text-sm">
-                 Descricao
-                </div>
-
-            </div>
-
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-
-                <div class="flex">
-
-                    <div class="w-1/3">Imagem</div>
-
-                    <div>
-
-                        <a href="/livro.php?" class="font-semibold hover:underline">Titulo</a>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
-
-                    </div>
-
-                </div>
-
-                <div class="text-sm">
-                 Descricao
-                </div>
-
-            </div>
-
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-
-                <div class="flex">
-
-                    <div class="w-1/3">Imagem</div>
-
-                    <div>
-
-                        <a href="/livro.php?" class="font-semibold hover:underline">Titulo</a>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
-
-                    </div>
-
-                </div>
-
-                <div class="text-sm">
-                 Descricao
-                </div>
-
-            </div>
-
-            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-
-                <div class="flex">
-
-                    <div class="w-1/3">Imagem</div>
-
-                    <div>
-
-                        <a href="/livro.php?" class="font-semibold hover:underline">Titulo</a>
-                        <div class="text-xs italic">Autor</div>
-                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
-
-                    </div>
-
-                </div>
-
-                <div class="text-sm">
-                 Descricao
-                </div>
-
-            </div>
+            <?php endforeach; ?>
 
             </section>
         </main>
