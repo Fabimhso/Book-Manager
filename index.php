6e7ea3796800815bef58a618e7dc433ce02e349e
Fabimhso <hsofabi05@gmail.com>
2024-12-18 16:51:30 -0300

    Adjusting Book Columns

--- index.php
+++ index.php
@@ -31,11 +31,123 @@
                 <button type="submit">🔎</button>
             </form>
 
-            <section class="space-y-4">
+            <!-- Lista de Livros -->
+
+            <section class="grid gap-4 grid-cols-3">
             
             <!-- LIVROS -->
 
-            <div class="w-1/3 p-2 rounded border-stone-800 border-2 bg-stone-900">
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+                <div class="flex">
+
+                    <div class="w-1/3">Imagem</div>
+
+                    <div>
+
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
+
+                    </div>
+
+                </div>
+
+                <div class="text-sm">
+                 Descricao
+                </div>
+
+            </div>
+
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+                <div class="flex">
+
+                    <div class="w-1/3">Imagem</div>
+
+                    <div>
+
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
+
+                    </div>
+
+                </div>
+
+                <div class="text-sm">
+                 Descricao
+                </div>
+
+            </div>
+
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+                <div class="flex">
+
+                    <div class="w-1/3">Imagem</div>
+
+                    <div>
+
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
+
+                    </div>
+
+                </div>
+
+                <div class="text-sm">
+                 Descricao
+                </div>
+
+            </div>
+
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+                <div class="flex">
+
+                    <div class="w-1/3">Imagem</div>
+
+                    <div>
+
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
+
+                    </div>
+
+                </div>
+
+                <div class="text-sm">
+                 Descricao
+                </div>
+
+            </div>
+
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+
+                <div class="flex">
+
+                    <div class="w-1/3">Imagem</div>
+
+                    <div>
+
+                        <div class="font-semibold">Titulo</div>
+                        <div class="text-xs italic">Autor</div>
+                        <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliacoes)</div>    
+
+                    </div>
+
+                </div>
+
+                <div class="text-sm">
+                 Descricao
+                </div>
+
+            </div>
+
+            <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
 
                 <div class="flex">
 
