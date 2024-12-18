7794e73c40691faf41e7275d852f5d712a3a7671
Fabimhso <hsofabi05@gmail.com>
2024-12-18 18:40:47 -0300

    Adjustments to PHP and creation of DADOS.PHP for DATA

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php 
 
-$livros = [
-    ['id' => 1, 'titulo' => 'A Vida Invisivel de ADDIE LARUE', 'autor' => 'V.E. SCHWAB', 'descricao' => 'A Menina que fez um pacto com a escuridao e foi esquecida por todos'],
-    ['id' => 2, 'titulo' => 'Harry Potter', 'autor' => 'J.K Rowling', 'descricao' => 'O Garoto que foi admitido na escola para bruxos Hogwarts'],
-    ['id' => 3, 'titulo' => 'A Menina que Roubava Livros', 'autor' => 'Markus Zusak', 'descricao' => 'A Garota que encontrou a morte 3 vezes e sobreviveu todas'],
-    ['id' => 4, 'titulo' => 'Crepusculo', 'autor' => 'Stephenie Meyer', 'descricao' => 'O Vampiro que Brilha no Sol'],
-    ['id' => 5, 'titulo' => 'Para Todos os Garotos Que Ja Amei', 'autor' => 'Jenny Han', 'descricao' => 'A Garota que Ama ate o Cara do Gas'],
-    ['id' => 6, 'titulo' => 'Mr. Robot: Red Wheelbarrow', 'autor' => 'Sam Esmail', 'descricao' => 'O Mlk que Hackeou ate tua Mae'],
-    ['id' => 7, 'titulo' => 'O Manual do Mundo', 'autor' => 'Ibere Tenorio', 'descricao' => 'O Manual de Como Hackear o Mundo'],
-    ['id' => 8, 'titulo' => 'Enaldinho - O Livro', 'autor' => 'Enaldo', 'descricao' => 'Livro do YouTuber Enaldinho, Enaldo, Enaldo, Enaldo'],
-    ['id' => 9, 'titulo' => 'Manual de Assassinato para Boas Garotas', 'autor' => 'Holly Jackson', 'descricao' => 'Como assassinar e continuar sendo boazinha e tirar boas notas']
-];
+require 'dados.php';
+
 ?>
 
 
